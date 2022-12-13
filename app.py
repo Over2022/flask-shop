f190b2cf5daf2da6e675c4032b5a64be7ac66bae
over2022 <1912977@mil.ru>
2022-12-13 14:56:35 +0300

    start

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/draft/')
+def draft():
+    return render_template('draft.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
