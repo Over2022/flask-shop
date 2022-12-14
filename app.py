5a9c7c40f92467862096d0d8db847c3306d6ea86
over2022 <1912977@mil.ru>
2022-12-14 13:44:59 +0300

    styles

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///shop.db'
 
 
 @app.route('/')
